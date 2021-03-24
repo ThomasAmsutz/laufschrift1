b0032352e2362b16668f58ff13a53e77e39aece3
ThomasAmsutz <79990384+ThomasAmsutz@users.noreply.github.com>
2021-03-24 20:47:36 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
+basic.showString("Lieber Elias, alles Gute zum 6. Geburtstag")
 basic.forever(function () {
-	
+    if (input.logoIsPressed()) {
+        basic.showIcon(IconNames.Heart)
+    } else if (input.logoIsPressed()) {
+        basic.showIcon(IconNames.Ghost)
+    } else if (input.buttonIsPressed(Button.A)) {
+        basic.showIcon(IconNames.EigthNote)
+    } else if (input.buttonIsPressed(Button.B)) {
+        basic.showIcon(IconNames.Snake)
+    } else {
+        basic.clearScreen()
+    }
 })
